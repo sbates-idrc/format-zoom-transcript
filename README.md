f868721c5bd282b876bc19ff9bbf157cd9febdb6
Simon Bates <sbates@ocadu.ca>
2026-02-18 10:13:00 -0500

    Add command line args processing

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This utility combines them together under a single timestamp.
 [Bob] 10:00:20
 That sounds handy!
 
-$ node format-zoom-transcript.js < example.txt | fold -s -w 72
+$ node format-zoom-transcript.js --blank-line-before-timestamp < example.txt | fold -s -w 72
 [Alice] 10:00:00
 Zoom transcripts may have multiple consecutive timestamped entries for
 a single speaker. This utility combines them together under a single
--- format-zoom-transcript.js
+++ format-zoom-transcript.js
@@ -2,6 +2,7 @@ import {ZoomTranscriptFormatter} from './zoom-transcript-formatter.js';
 import * as process from 'node:process';
 import * as readline from 'node:readline';
 import {Readable, Writable} from 'node:stream';
+import {parseArgs} from 'node:util';
 
 /**
  * Reads a Zoom transcript from 'input' and writes a formatted version
@@ -30,6 +31,37 @@ function formatTranscript(input, output, formatterOptions) {
 	});
 }
 
-formatTranscript(process.stdin, process.stdout, {
-	blankLineBeforeTimestamp: true,
-});
+/**
+ * Displays the program help text.
+ */
+function displayHelp() {
+	console.log('Usage: node format-zoom-transcript.js [options]');
+	console.log();
+	console.log('Options:');
+	console.log();
+	console.log('--blank-line-before-timestamp   Insert a blank line before each timestamp');
+	console.log('-h, --help                      Display this help text');
+	console.log();
+}
+
+const options = {
+	'blank-line-before-timestamp': {
+		type: 'boolean',
+		default: false,
+	},
+	help: {
+		type: 'boolean',
+		short: 'h',
+		default: false,
+	},
+};
+
+const {values} = parseArgs({options});
+
+if (values.help) {
+	displayHelp();
+} else {
+	formatTranscript(process.stdin, process.stdout, {
+		blankLineBeforeTimestamp: values['blank-line-before-timestamp'],
+	});
+}
